c9afb2e9f8caafa63a28b811259ec086349fe1e1
Ryusei Wada <wada@emol.jp>
2021-11-30 17:43:52 +0900

    feat: orientation lock

--- Hinami/HinamiApp.swift
+++ Hinami/HinamiApp.swift
@@ -43,6 +43,12 @@ class AppDelegate: NSObject, UIApplicationDelegate {
         NotificationController().removeSupplyNotification()
         print("フリックしてアプリを終了させた時に呼ばれる")
     }
+    
+    static var orientationLock = UIInterfaceOrientationMask.portrait //By default you want all your views to rotate freely
+    
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return AppDelegate.orientationLock
+    }
 }
 
 extension AppDelegate: UNUserNotificationCenterDelegate {
